0e05296acbd81a40bd3fd4350018c643a3c0d335
Robert Holak <rholak@codefriendly.com>
2018-05-10 11:13:16 -0500

    flipped path order

--- brew.plugin.zsh
+++ brew.plugin.zsh
@@ -1,4 +1,4 @@
-test -d ~/.linuxbrew && PATH="$HOME/.linuxbrew/bin:$HOME/.linuxbrew/sbin:$PATH"
 test -d /home/linuxbrew/.linuxbrew && PATH="/home/linuxbrew/.linuxbrew/bin:/home/linuxbrew/.linuxbrew/sbin:$PATH"
+test -d ~/.linuxbrew && PATH="$HOME/.linuxbrew/bin:$HOME/.linuxbrew/sbin:$PATH"
 
 export PATH=$(brew --prefix)/bin:$(brew --prefix)/sbin:$PATH
