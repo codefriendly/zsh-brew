4710d8b28341566e9710624906b8d39fa0f91250
Robert Holak <rholak@codefriendly.com>
2018-05-10 11:14:55 -0500

    too many paths. fixed

--- brew.plugin.zsh
+++ brew.plugin.zsh
@@ -1,4 +1,5 @@
+ORIG_PATH=$PATH
 test -d /home/linuxbrew/.linuxbrew && PATH="/home/linuxbrew/.linuxbrew/bin:/home/linuxbrew/.linuxbrew/sbin:$PATH"
 test -d ~/.linuxbrew && PATH="$HOME/.linuxbrew/bin:$HOME/.linuxbrew/sbin:$PATH"
 
-export PATH=$(brew --prefix)/bin:$(brew --prefix)/sbin:$PATH
+export PATH=$(brew --prefix)/bin:$(brew --prefix)/sbin:$ORIG_PATH
